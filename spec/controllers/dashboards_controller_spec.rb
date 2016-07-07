af3ec003596b4a22532d28d939497d66b1ab6a78
Josh McArthur <joshua.mcarthur@gmail.com>
2016-07-07 13:55:04 +1200

    Add test for dashboard controller

---
+++ spec/controllers/dashboards_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe DashboardsController, type: :controller do
+  describe "GET show" do
+    subject { get(:show) }
+    it { expect(subject).to be_ok }
+  end
+end
