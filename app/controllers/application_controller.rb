63ac99230eb32e8790b481d9447d815009836b6e
Josh McArthur <joshua.mcarthur@gmail.com>
2016-08-02 11:32:56 +1200

    Require user authentication for any controllers other than devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, unless: :devise_controller?
   layout :layout_by_resource
 
   protected
