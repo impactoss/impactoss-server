09724f5a1442d714aa787ef6ac77d6d677519ea9
Josh McArthur <joshua.mcarthur@gmail.com>
2016-08-02 11:29:23 +1200

    Mount letter_opener_web in development only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: "dashboards#show"
 end
