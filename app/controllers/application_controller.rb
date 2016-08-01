188f4e1ea4cbbe054f4b78f64385ae70d6ce094d
Josh McArthur <joshua.mcarthur@gmail.com>
2016-08-01 15:30:07 +1200

    Add layout for authentication views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    devise_controller? ? "authentication" : "application"
+  end
 end
