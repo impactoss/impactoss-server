8033cfa9382a47fc2619b7734017d88d018c676f
Josh McArthur <joshua.mcarthur@gmail.com>
2016-08-02 12:42:34 +1200

    Fix dodgy code from the internet

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
 module ControllerMacros
-  def sign_in(user = double('user'))
+  # rubocop:disable Metrics/AbcSize
+  def sign_in(user = double("user"))
     if user.nil?
-      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
+      allow(request.env["warden"]).to receive(:authenticate!).and_throw(:warden, scope: :user)
       allow(controller).to receive(:current_user).and_return(nil)
     else
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(request.env["warden"]).to receive(:authenticate!).and_return(user)
       allow(controller).to receive(:current_user).and_return(user)
     end
   end
