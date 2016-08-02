d72ccf61b99d13cafb81e5afe43670facdf53196
Josh McArthur <joshua.mcarthur@gmail.com>
2016-08-02 12:39:03 +1200

    Dashboards controller requires authenticated user to access

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 RSpec.describe DashboardsController, type: :controller do
   describe "GET show" do
     subject { get(:show) }
+    before { sign_in }
     it { expect(subject).to be_ok }
   end
 end
