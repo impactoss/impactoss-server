0d7b6a835dcaec1feef2f586bd55146ef6d9589c
Brenda Wallace <brenda@wallace.net.nz>
2016-11-16 12:19:54 +1300

    Add pundit to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!, unless: :devise_controller?
   layout :layout_by_resource
 
@@ -8,5 +12,12 @@ class ApplicationController < ActionController::Base
 
   def layout_by_resource
     devise_controller? ? "authentication" : "application"
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to(request_referrer || root_path)
   end
 end
