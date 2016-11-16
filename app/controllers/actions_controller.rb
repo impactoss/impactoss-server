17dba355f378db8f8409ae12bdfab370e8ff9d71
Ben Bradshaw <ben@rabidtech.co.nz>
2016-11-16 13:26:59 +1300

    Added controllers that need pundit

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,2 +1,38 @@
 class ActionsController < ApplicationController
+  def new
+    @action = Action.new
+    authorize @action
+  end
+
+  def index
+    @actions = policy_scope(Action)
+    authorize @actions
+  end
+
+  def edit
+    @action = Action.find(params[:id])
+    authorize @action
+  end
+
+  def update
+    @action = Action.find(params[:id])
+    authorize @action
+
+    if @action.update_attributes(permitted_attributes(@action))
+      redirect_to action_path, notice: "Action updated"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @action = Action.find(params[:id])
+    authorize @action
+
+    if @action.destroy
+      redirect_to actions_path, notice: "Action deleted"
+    else
+      redirect_to actions_path, notice: "Unable to delete Action"
+    end
+  end
 end
--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,38 @@
 class RecommendationsController < ApplicationController
+  def new
+    @recommendation = Recommendation.new
+    authorize @recommendation
+  end
+
+  def index
+    @recommendations = policy_scope(Recommendation)
+    authorize @recommendations
+  end
+
+  def edit
+    @recommendation = Recommendation.find(params[:id])
+    authorize @recommendation
+  end
+
+  def update
+    @recommendation = Recommendation.find(params[:id])
+    authorize @recommendation
+
+    if @recommendation.update_attributes(permitted_attributes(@recommendation))
+      redirect_to recommendation_path, notice: "Recommendation updated"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @recommendation = Recommendation.find(params[:id])
+    authorize @recommendation
+
+    if @recommendation.destroy
+      redirect_to recommendations_path, notice: "Recommendation deleted"
+    else
+      redirect_to recommendations_path, notice: "Unable to delete Recommendation"
+    end
+  end
 end
