504a76b697370d3c47069eac763915653e0bb6db
Brenda Wallace <brenda@wallace.net.nz>
2016-11-16 18:40:32 +1300

    Specs for Users#edit controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe UsersController, type: :controller do
       it { expect(subject).to be_ok }
     end
   end
+
+  describe "GET edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    subject { get :edit, params: { id: user.to_param } }
+    context "not signed in" do
+      it { expect(subject).not_to be_ok }
+    end
+    context "is signed in, but not admin" do
+      before { sign_in }
+      it { expect(subject).not_to be_ok }
+    end
+    context "is signed in as admin" do
+      before { sign_in_admin }
+      it { expect(subject).to be_ok }
+    end
+  end
 end
