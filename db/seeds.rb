dc3c5974f4846d38edd35f5658f7961a60daf2d8
Marian Clements <marian@rabidtech.co.nz>
2016-11-22 14:10:31 +1300

    rubocop opinions about " vs '

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ class Seeds
     return unless User.count.zero?
     FactoryGirl.create(:user).tap do |user|
       log "Seed user created: Log in with #{user.email} and password #{user.password}"
-      user.roles << Role.find_by(name: "manager")
+      user.roles << Role.find_by(name: 'manager')
       user.save!
     end
   end
--- spec/features/recommendation_spec.rb
+++ spec/features/recommendation_spec.rb
@@ -1,49 +1,49 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "User can see recommendation views", type: :feature do
+RSpec.feature 'User can see recommendation views', type: :feature do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:recommendations) { FactoryGirl.create_list(:recommendation, 5) }
 
   before(:each) do
-    admin = Role.new(name: "admin", friendly_name: "Admin")
+    admin = Role.new(name: 'admin', friendly_name: 'Admin')
     admin.save!
     user.roles = [admin]
   end
 
-  scenario "User can log in and see index page" do
+  scenario 'User can log in and see index page' do
     login_as(user)
     visit recommendations_path
-    expect(page).to have_content("Recommendations")
+    expect(page).to have_content('Recommendations')
     expect(page).to have_content(recommendations.first.title)
     expect(page).to have_content(recommendations.first.number)
-    expect(page).to have_content("New")
+    expect(page).to have_content('New')
   end
 
-  scenario "User can log in and see show page" do
+  scenario 'User can log in and see show page' do
     login_as(user)
     visit recommendation_path(recommendations.first)
-    expect(page).to have_content("Recommendations")
+    expect(page).to have_content('Recommendations')
     expect(page).to have_content(recommendations.first.title)
     expect(page).to have_content(recommendations.first.number)
-    expect(page).to have_content("Edit")
+    expect(page).to have_content('Edit')
   end
 
-  scenario "User can log in and see new page" do
+  scenario 'User can log in and see new page' do
     login_as(user)
 
     visit new_recommendation_path(Recommendation.first)
-    expect(page).to have_content("Recommendations")
-    expect(page).to have_button("Save")
-    expect(page).to have_content("Cancel")
+    expect(page).to have_content('Recommendations')
+    expect(page).to have_button('Save')
+    expect(page).to have_content('Cancel')
   end
 
-  scenario "User can log in and see edit page" do
+  scenario 'User can log in and see edit page' do
     login_as(user)
     visit edit_recommendation_path(recommendations.first)
-    expect(page).to have_content("Recommendations")
-    expect(find_field("Title").value).to eq(recommendations.first.title)
-    expect(find_field("Number").value).to eq(recommendations.first.number.to_s)
-    expect(page).to have_button("Save")
-    expect(page).to have_content("Cancel")
+    expect(page).to have_content('Recommendations')
+    expect(find_field('Title').value).to eq(recommendations.first.title)
+    expect(find_field('Number').value).to eq(recommendations.first.number.to_s)
+    expect(page).to have_button('Save')
+    expect(page).to have_content('Cancel')
   end
 end
