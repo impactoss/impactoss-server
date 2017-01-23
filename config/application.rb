a67e648bda9d44c8c1460f7fe5a37cfb3a942e8e
Christopher Baran <cb@chrisbaran.net>
2017-01-23 15:32:10 +1300

    Updated CORS to accept other headers

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HumanRightsNationalReporting
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
       end
     end
   end
