1f92fe919bb63c1c4bec77a05dbe3dd83ac0cc9a
Christopher Baran <cb@chrisbaran.net>
2017-01-23 16:15:44 +1300

    Loosened CORS permissions

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HumanRightsNationalReporting
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
+        resource '*', headers: :any, methods: :any
       end
     end
   end
