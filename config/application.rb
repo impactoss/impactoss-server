6ade2eda7b3bd693caef6c866eb5869415a0a615
Timo Franz <timo@dumpark.com>
2017-01-24 20:51:25 +1300

    fix again: explicitly expose cors response headers

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HumanRightsNationalReporting
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*',
-        resource '*', headers: :any, methods: :any, expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        resource '*', headers: :any, methods: :any, expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
       end
     end
   end
