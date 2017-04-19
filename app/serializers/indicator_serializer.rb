f4acd50271b9d0af5f4c116676aaebc85c2d9151
tmfrnz <timo@dumpark.com>
2017-04-20 11:08:34 +1200

    add new indicator fields to serialzer

--- app/serializers/indicator_serializer.rb
+++ app/serializers/indicator_serializer.rb
@@ -1,3 +1,3 @@
 class IndicatorSerializer < ApplicationSerializer
-  attributes :title, :description, :draft, :manager_id
+  attributes :title, :description, :draft, :manager_id, :frequency_months, :start_date, :repeat, :end_date
 end
