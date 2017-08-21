c92a078e49b6675336923d57a601e716667243bf
Christopher Baran <chris@rabidtech.co.nz>
2017-08-21 17:54:59 +1200

    performace test setup

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,9 +3,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def last_modified_user_id
-    return nil unless respond_to? :versions
-    return nil unless versions.last
-    versions.last.whodunnit
+    return 1
+    # return nil unless respond_to? :versions
+    # return nil unless versions.last
+    # versions.last.whodunnit
   end
 
   def last_modified_user
