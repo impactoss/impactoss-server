ef6ddcd7570b6ab049d460078aa3f952b9b21e59
Samu Jukkola <samujukkola@gmail.com>
2019-12-12 18:27:26 +1300

    Add tests for sub relations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { should validate_presence_of :title }
   it { should belong_to :taxonomy }
+  it { should belong_to :category }
+  it { should have_many :categories }
   it { should belong_to :manager }
   it { should have_many :recommendations }
   it { should have_many :users }
@@ -10,4 +12,28 @@ RSpec.describe Category, type: :model do
   it { should have_many :indicators }
   it { should have_many :progress_reports }
   it { should have_many :due_dates }
+
+  context "Sub-relation validations" do
+    it "Should update parent_id with correct taxonomy relation" do
+      category = FactoryGirl.create(:category, :parent_category)
+      sub_category = FactoryGirl.create(:category, :sub_category)
+      taxonomy = FactoryGirl.create(:taxonomy, :sub_taxonomy)
+
+      sub_category.taxonomy_id = taxonomy.id
+      category.taxonomy_id = taxonomy.parent_id
+      sub_category.save!
+      category.save!
+
+      sub_category.parent_id = category.id
+      sub_category.save!
+
+    end
+
+    it "Should not update parent_id with incorrect taxonomy relation" do
+      category = FactoryGirl.create(:category, :parent_category)
+      sub_category = FactoryGirl.create(:category, :sub_category)
+      sub_category.parent_id = category.id
+      expect{sub_category.save!}.to raise_exception(/Validation failed: Parent Taxonomy does not have parent categorys taxonomy as parent./)
+    end
+  end
 end
--- spec/models/taxonomy_spec.rb
+++ spec/models/taxonomy_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Taxonomy, type: :model do
   it { should validate_presence_of :title }
+  it { should belong_to :taxonomy }
+  it { should have_many :taxonomies }
   it { should validate_inclusion_of(:allow_multiple).in_array([true, false]) }
   it { should validate_inclusion_of(:tags_recommendations).in_array([true, false]) }
   it { should validate_inclusion_of(:tags_measures).in_array([true, false]) }
