665a4ff69144e4a3e3f3144141870ef1aba8e955
Samu Jukkola <samujukkola@gmail.com>
2019-12-28 19:12:30 +1300

    Remove sub-relations from serializers

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,6 +1,6 @@
 class CategorySerializer
   include FastVersionedSerializer
-  attributes :title, :categories, :parent_id, :short_title, :description, :url, :draft, :reference, :taxonomy_id, :manager_id, :date, :user_only
+  attributes :title, :parent_id, :short_title, :description, :url, :draft, :reference, :taxonomy_id, :manager_id, :date, :user_only
 
   set_type :categories
 end
