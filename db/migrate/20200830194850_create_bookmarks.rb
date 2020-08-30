25358608bc43fa2fe9b34ce44815d7379a18e4ff
Andrés Proaño Valencia <andres@oventi.org>
2020-08-30 20:25:40 +1200

    added migration

---
+++ db/migrate/20200830194850_create_bookmarks.rb
@@ -0,0 +1,12 @@
+class CreateBookmarks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookmarks do |t|
+      t.belongs_to :user, index: true, null: false
+
+      t.string :title, null: false
+      t.json :view, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180528083426) do
+ActiveRecord::Schema.define(version: 20200830194850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "title",      null: false
+    t.json     "view",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
+  end
 
   create_table "categories", force: :cascade do |t|
     t.text     "title"
