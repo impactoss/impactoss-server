8eea578ad88b67dc9e4cb506bd0f8402d7eaa769
Nicola Rankin <nicola.rankin@gmail.com>
2020-12-10 16:14:11 +1300

    change from deprecated should syntax

--- spec/models/framework_spec.rb
+++ spec/models/framework_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Framework, type: :model do
-  it { should validate_presence_of :title }
-  it { should have_many :frameworks }
-  it { should have_many :framework_taxonomies }
-  it { should have_many :taxonomies }
-  it { should have_many :recommendations }
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to have_many :frameworks }
+  it { is_expected.to have_many :framework_taxonomies }
+  it { is_expected.to have_many :taxonomies }
+  it { is_expected.to have_many :recommendations }
+  it { is_expected.to have_many :framework_frameworks }
 end
--- spec/models/framework_taxonomy_spec.rb
+++ spec/models/framework_taxonomy_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FrameworkTaxonomy, type: :model do 
-  it { should belong_to :framework }
-  it { should belong_to :taxonomy }
+  it { is_expected.to belong_to :framework }
+  it { is_expected.to belong_to :taxonomy }
 end
