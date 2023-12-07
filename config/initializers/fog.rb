a41a7436000905d90b7bfa4b4e8d0364300fc5d2
Philip Arndt <git@p.arndt.io>
2023-12-08 09:50:37 +1300

    Skip Fog storage configuration when we don't have the access keys

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,6 +1,10 @@
-::FogStorage = Fog::Storage.new(
-  provider: 'AWS',
-  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: ENV['AWS_BUCKET_REGION']
-)
+if ENV["AWS_ACCESS_KEY_ID"] && ENV["AWS_SECRET_ACCESS_KEY"] && ENV["AWS_BUCKET_REGION"]
+  ::FogStorage = Fog::Storage.new(
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_BUCKET_REGION']
+  )
+else
+  warn "[config/initializers/fog.rb] Missing AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_BUCKET_REGION environment variables."
+end
