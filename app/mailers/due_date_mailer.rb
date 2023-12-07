bf1cc48d452b24ce9bad710573b7fd3fb6e8e2cd
tmfrnz <timo@dumpark.com>
2023-12-07 16:40:08 +0100

    update due date mailers

--- app/mailers/due_date_mailer.rb
+++ app/mailers/due_date_mailer.rb
@@ -7,19 +7,19 @@ class DueDateMailer < ApplicationMailer
   def due(due_date)
     return unless due_date.manager_email
     @indicator = due_date.indicator
-    @due_date = due_date
+    @due_date = due_date.due_date
     @manager_name = due_date.manager_name
+    @client_url = ENV["CLIENT_URL"] || 'https://undefined.client.url'
 
     mail to: due_date.manager_email, subject: I18n.t("due_date_mailer.due.subject")
   end
 
   def category_due(due_date, category)
-    return unless category.manager
+    return unless category.manager_email
     @indicator = due_date.indicator
     @category = category
-    @category_title = category.title
-    @due_date = due_date
-    @manager_name = category.manager_name
+    @due_date = due_date.due_date
+    @client_url = ENV["CLIENT_URL"] || 'https://undefined.client.url'
 
     mail to: category.manager_email, subject: I18n.t("due_date_mailer.category_due.subject")
   end
@@ -32,19 +32,19 @@ class DueDateMailer < ApplicationMailer
   def overdue(due_date)
     return unless due_date.manager_email
     @indicator = due_date.indicator
-    @due_date = due_date
+    @due_date = due_date.due_date
     @manager_name = due_date.manager_name
+    @client_url = ENV["CLIENT_URL"] || 'https://undefined.client.url'
 
     mail to: due_date.manager_email, subject: I18n.t("due_date_mailer.overdue.subject")
   end
 
   def category_overdue(due_date, category)
-    return unless category.manager
+    return unless category.manager_email
     @indicator = due_date.indicator
     @category = category
-    @category_title = category.title
-    @due_date = due_date
-    @manager_name = category.manager_name
+    @due_date = due_date.due_date
+    @client_url = ENV["CLIENT_URL"] || 'https://undefined.client.url'
 
     mail to: category.manager_email, subject: I18n.t("due_date_mailer.category_overdue.subject")
   end
