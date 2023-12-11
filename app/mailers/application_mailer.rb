9b5bb6c0803b5daac10e7d008dc25d912c5c8b93
tmfrnz <timo@dumpark.com>
2023-12-11 15:42:45 +0100

    use  email_address_with_nameand environment variables in ActionMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@mail.impactoss.org"
+  default from: email_address_with_name(ENV["ACTION_MAILER_FROM_EMAIL"] || "no-reply@mail.impactoss.org", ENV["ACTION_MAILER_FROM_NAME"] || "IMPACT OSS Notifications")
   layout "mailer"
 end
