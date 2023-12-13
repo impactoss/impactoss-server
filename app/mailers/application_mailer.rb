e40f54fef589dc276568e8b9fdb4dca44f410f3a
Timo Franz <timo@dumpark.com>
2023-12-13 18:16:58 +0100

    Update app/mailers/application_mailer.rb
    Co-authored-by: Philip Arndt <git@p.arndt.io>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(ENV["ACTION_MAILER_FROM_EMAIL"] || "no-reply@mail.impactoss.org", ENV["ACTION_MAILER_FROM_NAME"] || "IMPACT OSS Notifications")
+  default from: email_address_with_name(
+    ENV.fetch("ACTION_MAILER_FROM_EMAIL", "no-reply@mail.impactoss.org"),
+    ENV.fetch("ACTION_MAILER_FROM_NAME", "IMPACT OSS Notifications")
+  )
   layout "mailer"
 end
