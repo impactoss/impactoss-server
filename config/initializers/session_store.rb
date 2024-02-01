c3885ef870fd6ac4495952046d84a2945d038e06
Philip Arndt <git@p.arndt.io>
2024-02-01 21:44:43 +1300

    Attempt to configure a session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,3 +3,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: "_human-rights-national-reporting_session"
+Rails.application.config.middleware.use ActionDispatch::Cookies
+Rails.application.config.middleware.use ActionDispatch::Session::CookieStore, Rails.application.config.session_options
