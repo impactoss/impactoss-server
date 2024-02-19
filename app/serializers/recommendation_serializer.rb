1a465112214d0d2ad30ef545456a7f8ec29d3781
tmfrnz <timo@dumpark.com>
2024-02-19 19:53:54 +1300

    add relationship updated fields to recommendation seralizer #362

--- app/serializers/recommendation_serializer.rb
+++ app/serializers/recommendation_serializer.rb
@@ -1,7 +1,7 @@
 class RecommendationSerializer
   include FastVersionedSerializer
 
-  attributes :title, :accepted, :response, :draft, :reference, :description, :framework_id
+  attributes :title, :accepted, :response, :draft, :reference, :description, :framework_id, :relationship_updated_at, :relationship_updated_by_id
 
   set_type :recommendations
 end
