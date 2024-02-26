87fbfa0cd03632e190bab6acf038dbbfa237f9a4
tmfrnz <timo@dumpark.com>
2024-02-26 22:06:07 +1300

    test cors header 2

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module HumanRightsNationalReporting
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://sadata-staging.web.app'
         resource '*', headers: :any, methods: :any, expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
       end
     end
