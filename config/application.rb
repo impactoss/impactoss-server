64f74cca234dbd888212c404eab3f682c860ef66
tmfrnz <timo@dumpark.com>
2024-02-27 19:42:48 +1300

    try multiple origins, incl localhost

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module HumanRightsNationalReporting
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://sadata-staging.web.app'
+        origins 'localhost:3000', 'https://sadata-staging.web.app'
         resource '*', headers: :any, methods: :any, expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], credentials: true
       end
     end
