92d3d837fd1cadd96e11fb83a13d984a3911cc65
tmfrnz <timo@dumpark.com>
2024-03-08 09:37:55 +1200

    add s3 allowed origin for s3 upload

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,19 @@ module HumanRightsNationalReporting
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "*"
-        resource "*", headers: :any, methods: :any, expose: ["access-token", "expiry", "token-type", "uid", "client"]
+        origins ENV['ALLOWED_ORIGIN_S3'].split(',').map { |origin| origin.strip }
+        resource '/s3/*',
+                  headers: :any,
+                  methods: :any,
+                  expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                  credentials: true
+      end
+      allow do
+        origins '*'
+        resource '*',
+                  headers: :any,
+                  methods: :any,
+                  expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
       end
     end
 
