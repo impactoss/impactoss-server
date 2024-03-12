4833bd552c8acf415568583ece95228822c3cd65
tmfrnz <timo@dumpark.com>
2024-03-13 10:51:00 +1200

    return alternative URL

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -6,7 +6,11 @@ class S3Controller < ApplicationController
     headers = {"Content-Type" => params[:contentType], "x-amz-acl" => "public-read-write"}
 
     url = FogStorage.put_object_url(ENV["S3_BUCKET_NAME"], "#{ENV["S3_ASSET_FOLDER"]}/#{params[:objectName]}", 15.minutes.from_now.to_time.to_i, headers, options)
-
-    render json: {signedUrl: url}
+    protocol_path = url.split('//')
+    path = protocol_path.last
+    path_query = path.split('?')
+    query = path_query[1]
+    url2 = "#{ENV["CLIENT_URL"]}/#{ENV["S3_ASSET_FOLDER"]}/#{params[:objectName]}?#{query}"
+    render json: {signedUrl: url, signedUrl2: url2}
   end
 end
