573c83038696fc97480333505ce2ff976ddcf278
Luke Arndt <git@luke.arndt.io>
2024-09-17 19:59:15 +1200

    Enforce allow_multiple on UserCategory

--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -6,9 +6,32 @@ class UserCategory < ApplicationRecord
   validates :user_id, presence: true
   validates :category_id, presence: true
 
+  after_commit :enforce_allow_multiple_from_taxonomy, on: [:create, :update]
   after_commit :set_relationship_updated, on: [:create, :update, :destroy]
 
   private
+
+  def disallow_multiple?
+    self.class
+      .joins(category: :taxonomy)
+      .where({
+        user_id: user_id,
+        category: {
+          taxonomies: {allow_multiple: false}
+        }
+      })
+      .count > 0
+  end
+
+  def enforce_allow_multiple_from_taxonomy
+    if disallow_multiple?
+      others_on_user.destroy_all
+    end
+  end
+
+  def others_on_user
+    self.class.where(user_id: user_id).where.not(category_id: category_id)
+  end
 
   def set_relationship_updated
     if user && !user.destroyed?
--- spec/models/user_category_spec.rb
+++ spec/models/user_category_spec.rb
@@ -43,4 +43,61 @@ RSpec.describe UserCategory, type: :model do
       expect { subject.destroy }.to change { user.reload.relationship_updated_by_id }.to(whodunnit)
     end
   end
+
+  context "category.taxonomy.allow_multiple" do
+    let(:user) { FactoryBot.create(:user) }
+
+    let(:taxonomy_allow) { FactoryBot.create(:taxonomy, allow_multiple: true, title: "Taxonomy: Allow Multiple") }
+    let(:taxonomy_disallow) { FactoryBot.create(:taxonomy, allow_multiple: false, title: "Taxonomy: Disallow Multiple") }
+
+    let(:category_allow_one) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 1", title: "Category: Allow Multiple, 1") }
+    let(:category_allow_two) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 2", title: "Category: Allow Multiple, 2") }
+
+    let(:category_disallow_one) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 1", title: "Category: Disallow Multiple, 1") }
+    let(:category_disallow_two) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 2", title: "Category: Disallow Multiple, 2") }
+
+    context "when user already has a category with taxonomy.allow_multiple: false" do
+      before do
+        described_class.create(category: category_disallow_one, user: user)
+      end
+
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(user.reload.categories).to match_array([category_disallow_one])
+
+        described_class.create(category: category_disallow_two, user: user)
+
+        expect(user.reload.categories).to match_array([category_disallow_two])
+      end
+
+      it "adding a second category with { allow_multiple: true } replaces the existing category" do
+        expect(user.reload.categories).to match_array([category_disallow_one])
+
+        described_class.create(category: category_allow_two, user: user)
+
+        expect(user.reload.categories).to match_array([category_allow_two])
+      end
+    end
+
+    context "when user already has a category with taxonomy.allow_multiple: true" do
+      before do
+        described_class.create(category: category_allow_one, user: user)
+      end
+
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(user.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_disallow_two, user: user)
+
+        expect(user.reload.categories).to match_array([category_disallow_two])
+      end
+
+      it "adding a second category with { allow_multiple: true } results in both categories" do
+        expect(user.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_allow_two, user: user)
+
+        expect(user.reload.categories).to match_array([category_allow_one, category_allow_two])
+      end
+    end
+  end
 end
