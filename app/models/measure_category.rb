05da50223a911349b6dde37e354bce9b5b2a1005
Luke Arndt <git@luke.arndt.io>
2024-09-17 19:59:06 +1200

    Enforce allow_multiple on MeasureCategory

--- app/models/measure_category.rb
+++ app/models/measure_category.rb
@@ -8,9 +8,32 @@ class MeasureCategory < VersionedRecord
   validates :measure_id, presence: true
   validates :category_id, presence: true
 
+  after_commit :enforce_allow_multiple_from_taxonomy, on: [:create, :update]
   after_commit :set_relationship_updated, on: [:create, :update, :destroy]
 
   private
+
+  def disallow_multiple?
+    self.class
+      .joins(category: :taxonomy)
+      .where({
+        measure_id: measure_id,
+        category: {
+          taxonomies: {allow_multiple: false}
+        }
+      })
+      .count > 0
+  end
+
+  def enforce_allow_multiple_from_taxonomy
+    if disallow_multiple?
+      others_on_measure.destroy_all
+    end
+  end
+
+  def others_on_measure
+    self.class.where(measure_id: measure_id).where.not(category_id: category_id)
+  end
 
   def set_relationship_updated
     if measure && !measure.destroyed?
--- spec/models/measure_category_spec.rb
+++ spec/models/measure_category_spec.rb
@@ -7,52 +7,101 @@ RSpec.describe MeasureCategory, type: :model do
   it { is_expected.to validate_presence_of :category_id }
   it { is_expected.to validate_presence_of :measure_id }
 
-  let(:category) { FactoryBot.create(:category) }
-  let(:measure) { FactoryBot.create(:measure) }
+  context "with default fixtures" do
+    let(:category) { FactoryBot.create(:category) }
+    let(:measure) { FactoryBot.create(:measure) }
+    let(:whodunnit) { FactoryBot.create(:user).id }
 
-  subject { described_class.create(category: category, measure: measure) }
+    subject { described_class.create(category: category, measure: measure) }
 
-  it "works" do
-    expect(subject).to be_valid
+    before { allow(::PaperTrail.request).to receive(:whodunnit).and_return(whodunnit) }
+
+    it "creates a valid record" do
+      expect(subject).to be_valid
+    end
+
+    it "create sets the relationship_updated_at on the measure" do
+      expect { subject }.to change { measure.reload.relationship_updated_at }
+    end
+
+    it "update sets the relationship_updated_at on the measure" do
+      subject
+      expect { subject.touch }.to change { measure.reload.relationship_updated_at }
+    end
+
+    it "destroy sets the relationship_updated_at on the measure" do
+      expect { subject.destroy }.to change { measure.reload.relationship_updated_at }
+    end
+
+    it "create sets the relationship_updated_by_id on the measure" do
+      expect { subject }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
+    end
+
+    it "update sets the relationship_updated_by_id on the measure" do
+      subject
+      measure.update_column(:relationship_updated_by_id, nil)
+      expect { subject.touch }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
+    end
+
+    it "destroy sets the relationship_updated_by_id on the measure" do
+      expect { subject.destroy }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
+    end
   end
 
-  it "create sets the relationship_updated_at on the measure" do
-    expect { subject }.to change { measure.reload.relationship_updated_at }
-  end
+  context "category.taxonomy.allow_multiple" do
+    let(:measure) { FactoryBot.create(:measure) }
 
-  let(:whodunnit) { FactoryBot.create(:user).id }
-  before { allow(::PaperTrail.request).to receive(:whodunnit).and_return(whodunnit) }
+    let(:taxonomy_allow) { FactoryBot.create(:taxonomy, allow_multiple: true, title: "Taxonomy: Allow Multiple") }
+    let(:taxonomy_disallow) { FactoryBot.create(:taxonomy, allow_multiple: false, title: "Taxonomy: Disallow Multiple") }
 
-  subject { described_class.create(category: category, measure: measure) }
+    let(:category_allow_one) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 1", title: "Category: Allow Multiple, 1") }
+    let(:category_allow_two) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 2", title: "Category: Allow Multiple, 2") }
 
-  it "works" do
-    expect(subject).to be_valid
-  end
+    let(:category_disallow_one) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 1", title: "Category: Disallow Multiple, 1") }
+    let(:category_disallow_two) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 2", title: "Category: Disallow Multiple, 2") }
 
-  it "create sets the relationship_updated_at on the measure" do
-    expect { subject }.to change { measure.reload.relationship_updated_at }
-  end
+    context "when measure already has a category with taxonomy.allow_multiple: false" do
+      before do
+        described_class.create(category: category_disallow_one, measure: measure)
+      end
 
-  it "update sets the relationship_updated_at on the measure" do
-    subject
-    expect { subject.touch }.to change { measure.reload.relationship_updated_at }
-  end
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(measure.reload.categories).to match_array([category_disallow_one])
 
-  it "destroy sets the relationship_updated_at on the measure" do
-    expect { subject.destroy }.to change { measure.reload.relationship_updated_at }
-  end
+        described_class.create(category: category_disallow_two, measure: measure)
 
-  it "create sets the relationship_updated_by_id on the measure" do
-    expect { subject }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
-  end
+        expect(measure.reload.categories).to match_array([category_disallow_two])
+      end
 
-  it "update sets the relationship_updated_by_id on the measure" do
-    subject
-    measure.update_column(:relationship_updated_by_id, nil)
-    expect { subject.touch }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
-  end
+      it "adding a second category with { allow_multiple: true } replaces the existing category" do
+        expect(measure.reload.categories).to match_array([category_disallow_one])
 
-  it "destroy sets the relationship_updated_by_id on the measure" do
-    expect { subject.destroy }.to change { measure.reload.relationship_updated_by_id }.to(whodunnit)
+        described_class.create(category: category_allow_two, measure: measure)
+
+        expect(measure.reload.categories).to match_array([category_allow_two])
+      end
+    end
+
+    context "when measure already has a category with taxonomy.allow_multiple: true" do
+      before do
+        described_class.create(category: category_allow_one, measure: measure)
+      end
+
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(measure.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_disallow_two, measure: measure)
+
+        expect(measure.reload.categories).to match_array([category_disallow_two])
+      end
+
+      it "adding a second category with { allow_multiple: true } results in both categories" do
+        expect(measure.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_allow_two, measure: measure)
+
+        expect(measure.reload.categories).to match_array([category_allow_one, category_allow_two])
+      end
+    end
   end
 end
