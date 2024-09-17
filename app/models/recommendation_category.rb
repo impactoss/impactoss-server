5f6fd0fe584926d0c84e14074d8873d87f15e7ac
Luke Arndt <git@luke.arndt.io>
2024-09-17 15:36:01 +1200

    Enforce allow_multiple on recommendations

--- app/models/recommendation_category.rb
+++ app/models/recommendation_category.rb
@@ -8,9 +8,32 @@ class RecommendationCategory < VersionedRecord
   validates :recommendation_id, presence: true
   validates :category_id, presence: true
 
+  after_commit :enforce_allow_multiple_from_taxonomy, on: [:create, :update]
   after_commit :set_relationship_updated, on: [:create, :update, :destroy]
 
   private
+
+  def disallow_multiple
+    RecommendationCategory
+      .joins(category: :taxonomy)
+      .where({
+        recommendation_id: recommendation_id,
+        category: {
+          taxonomies: {allow_multiple: false}
+        }
+      })
+      .count > 0
+  end
+
+  def enforce_allow_multiple_from_taxonomy
+    if disallow_multiple
+      other_recommendation_categories_on_recommendation.destroy_all
+    end
+  end
+
+  def other_recommendation_categories_on_recommendation
+    RecommendationCategory.where(recommendation_id: recommendation_id).where.not(category_id: category_id)
+  end
 
   def set_relationship_updated
     if recommendation && !recommendation.destroyed?
--- spec/models/recommendation_category_spec.rb
+++ spec/models/recommendation_category_spec.rb
@@ -7,52 +7,101 @@ RSpec.describe RecommendationCategory, type: :model do
   it { is_expected.to validate_presence_of(:recommendation_id) }
   it { is_expected.to validate_presence_of(:category_id) }
 
-  let(:category) { FactoryBot.create(:category) }
-  let(:recommendation) { FactoryBot.create(:recommendation) }
+  context "with default fixtures" do
+    let(:category) { FactoryBot.create(:category) }
+    let(:recommendation) { FactoryBot.create(:recommendation) }
+    let(:whodunnit) { FactoryBot.create(:user).id }
 
-  subject { described_class.create(category: category, recommendation: recommendation) }
+    subject { described_class.create(category: category, recommendation: recommendation) }
 
-  it "works" do
-    expect(subject).to be_valid
+    before { allow(::PaperTrail.request).to receive(:whodunnit).and_return(whodunnit) }
+
+    it "creates a valid record" do
+      expect(subject).to be_valid
+    end
+
+    it "create sets the relationship_updated_at on the recommendation" do
+      expect { subject }.to change { recommendation.reload.relationship_updated_at }
+    end
+
+    it "update sets the relationship_updated_at on the recommendation" do
+      subject
+      expect { subject.touch }.to change { recommendation.reload.relationship_updated_at }
+    end
+
+    it "destroy sets the relationship_updated_at on the recommendation" do
+      expect { subject.destroy }.to change { recommendation.reload.relationship_updated_at }
+    end
+
+    it "create sets the relationship_updated_by_id on the recommendation" do
+      expect { subject }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
+    end
+
+    it "update sets the relationship_updated_by_id on the recommendation" do
+      subject
+      recommendation.update_column(:relationship_updated_by_id, nil)
+      expect { subject.touch }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
+    end
+
+    it "destroy sets the relationship_updated_by_id on the recommendation" do
+      expect { subject.destroy }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
+    end
   end
 
-  it "create sets the relationship_updated_at on the recommendation" do
-    expect { subject }.to change { recommendation.reload.relationship_updated_at }
-  end
+  context "category.taxonomy.allow_multiple" do
+    let(:recommendation) { FactoryBot.create(:recommendation) }
 
-  let(:whodunnit) { FactoryBot.create(:user).id }
-  before { allow(::PaperTrail.request).to receive(:whodunnit).and_return(whodunnit) }
+    let(:taxonomy_allow) { FactoryBot.create(:taxonomy, allow_multiple: true, title: "Taxonomy: Allow Multiple") }
+    let(:taxonomy_disallow) { FactoryBot.create(:taxonomy, allow_multiple: false, title: "Taxonomy: Disallow Multiple") }
 
-  subject { described_class.create(category: category, recommendation: recommendation) }
+    let(:category_allow_one) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 1", title: "Category: Allow Multiple, 1") }
+    let(:category_allow_two) { FactoryBot.create(:category, taxonomy: taxonomy_allow, short_title: "Category Allow 2", title: "Category: Allow Multiple, 2") }
 
-  it "works" do
-    expect(subject).to be_valid
-  end
+    let(:category_disallow_one) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 1", title: "Category: Disallow Multiple, 1") }
+    let(:category_disallow_two) { FactoryBot.create(:category, taxonomy: taxonomy_disallow, short_title: "Category Disallow 2", title: "Category: Disallow Multiple, 2") }
 
-  it "create sets the relationship_updated_at on the recommendation" do
-    expect { subject }.to change { recommendation.reload.relationship_updated_at }
-  end
+    context "when recommendation already has a category with taxonomy.allow_multiple: false" do
+      before do
+        described_class.create(category: category_disallow_one, recommendation: recommendation)
+      end
 
-  it "update sets the relationship_updated_at on the recommendation" do
-    subject
-    expect { subject.touch }.to change { recommendation.reload.relationship_updated_at }
-  end
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(recommendation.reload.categories).to match_array([category_disallow_one])
 
-  it "destroy sets the relationship_updated_at on the recommendation" do
-    expect { subject.destroy }.to change { recommendation.reload.relationship_updated_at }
-  end
+        described_class.create(category: category_disallow_two, recommendation: recommendation)
 
-  it "create sets the relationship_updated_by_id on the recommendation" do
-    expect { subject }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
-  end
+        expect(recommendation.reload.categories).to match_array([category_disallow_two])
+      end
 
-  it "update sets the relationship_updated_by_id on the recommendation" do
-    subject
-    recommendation.update_column(:relationship_updated_by_id, nil)
-    expect { subject.touch }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
-  end
+      it "adding a second category with { allow_multiple: true } replaces the existing category" do
+        expect(recommendation.reload.categories).to match_array([category_disallow_one])
 
-  it "destroy sets the relationship_updated_by_id on the recommendation" do
-    expect { subject.destroy }.to change { recommendation.reload.relationship_updated_by_id }.to(whodunnit)
+        described_class.create(category: category_allow_two, recommendation: recommendation)
+
+        expect(recommendation.reload.categories).to match_array([category_allow_two])
+      end
+    end
+
+    context "when recommendation already has a category with taxonomy.allow_multiple: true" do
+      before do
+        described_class.create(category: category_allow_one, recommendation: recommendation)
+      end
+
+      it "adding a second category with { allow_multiple: false } replaces the existing category" do
+        expect(recommendation.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_disallow_two, recommendation: recommendation)
+
+        expect(recommendation.reload.categories).to match_array([category_disallow_two])
+      end
+
+      it "adding a second category with { allow_multiple: true } results in both categories" do
+        expect(recommendation.reload.categories).to match_array([category_allow_one])
+
+        described_class.create(category: category_allow_two, recommendation: recommendation)
+
+        expect(recommendation.reload.categories).to match_array([category_allow_one, category_allow_two])
+      end
+    end
   end
 end
