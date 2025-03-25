f4cc1e419ec1d1489e77f037e9cf278880113026
Jonathan Arndt <jonathan@manuka.co>
2025-03-25 15:28:36 +1300

    apply i18n config with fallbacks, to respect NZ date format

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module HumanRightsNationalReporting
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :'en-NZ'
+    config.i18n.fallbacks = { :'en-NZ' => :'en' }
   end
 end
