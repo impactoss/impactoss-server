50bb414070dd9f27c770645de379a4734fe086db
Jonathan Arndt <jonathan@manuka.co>
2025-03-28 00:26:41 +0000

    fixup! restore and upgrade puma settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,8 +2,6 @@
 
 require "active_support/core_ext/integer/time"
 
-
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/puma.rb
+++ config/puma.rb
@@ -31,7 +31,6 @@ environment ENV.fetch("RAILS_ENV", "development")
 #
 # workers ENV.fetch("WEB_CONCURRENCY", 2)
 
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
