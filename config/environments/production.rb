638e41dd47464c493b7fe616a57f227fcece3f10
tmfrnz <timo@dumpark.com>
2025-04-04 12:26:41 +0200

    remove unused asswets initialiser and references

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,12 +33,12 @@ Rails.application.configure do
 
   # Disable minification since it adds a *huge* amount of time to precompile.
   # Anyway, gzip alone gets us about 70% of the benefits of minify+gzip.
-  config.assets.js_compressor = false
-  config.assets.css_compressor = false
+  # config.assets.js_compressor = false
+  # config.assets.css_compressor = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  # config.assets.compile = false
+# 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
