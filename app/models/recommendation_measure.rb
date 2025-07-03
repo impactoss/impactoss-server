14c01b784245167a1bc9d701aa570ca5636b5c7f
tmfrnz <timo@dumpark.com>
2025-07-03 15:18:17 +0200

    add custom validation

--- app/models/recommendation_measure.rb
+++ app/models/recommendation_measure.rb
@@ -1,5 +1,5 @@
 class RecommendationMeasure < VersionedRecord
-  belongs_to :recommendation, inverse_of: :recommendation_measures
+  belongs_to :recommendation, inverse_of: :recommendation_measures, optional: true
   belongs_to :measure, inverse_of: :recommendation_measures
   accepts_nested_attributes_for :recommendation
   accepts_nested_attributes_for :measure
@@ -7,10 +7,17 @@ class RecommendationMeasure < VersionedRecord
   validates :measure_id, uniqueness: {scope: :recommendation_id}
   validates :recommendation_id, presence: true
   validates :measure_id, presence: true
+  validate :recommendation_must_exist
 
   after_commit :set_relationship_updated, on: [:create, :update, :destroy]
 
   private
+
+  def recommendation_must_exist
+    if recommendation_id.present? && !Recommendation.exists?(recommendation_id)
+      errors.add(:recommendation, "must exist (id: #{recommendation_id})")
+    end
+  end
 
   def set_relationship_updated
     if recommendation && !recommendation.destroyed?
