c3902f006cdafac402da69b59666f727da8ebcbd
tmfrnz <timo@dumpark.com>
2025-07-04 16:35:57 +0200

    check for CLIENT_URL in s3controller

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -9,7 +9,10 @@ class S3Controller < ApplicationController
 
     object_path = "#{ENV["S3_ASSET_FOLDER"]}/#{params[:objectName]}"
     s3_url = ::FogStorage.put_object_url(ENV["S3_BUCKET_NAME"], object_path, 15.minutes.from_now.to_time.to_i, headers, options)
-    url = "#{ENV["CLIENT_URL"]}/#{object_path}?#{URI(s3_url).query}"
+    url = s3_url
+    if ENV["CLIENT_URL"].present?
+      url = "#{ENV["CLIENT_URL"]}/#{object_path}?#{URI(s3_url).query}"
+    end
 
     render json: {signedUrl: url}
   end
