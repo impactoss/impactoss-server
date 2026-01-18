28a209afbafc1b55fd58f920b22e16419cb4de38
tmfrnz <timo@dumpark.com>
2026-01-18 14:54:01 +0100

    initial SG cont. fix features controller/skip pundit

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,4 +1,7 @@
 class FeaturesController < ApplicationController
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
+
   def index
     render json: {features: Features::FEATURES}
   end
