16f5615850bd72b64b28f8e19d232eb35ee8451f
tmfrnz <timo@dumpark.com>
2026-01-18 14:40:56 +0100

    initial SG cont. add features endpoint

---
+++ app/controllers/features_controller.rb
@@ -0,0 +1,5 @@
+class Api::FeaturesController < ApplicationController
+  def index
+    render json: { features: Features::FEATURES }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
   get "static_pages/home"
   get "s3/sign"
+  get 'features', to: 'features#index'
 
   resources :categories
   resources :recommendations
