82f7a8653eeb2750f75fa4b7e2963b4aced04bf9
tmfrnz <tmfrnztimo@dumpark.com>
2026-02-24 09:15:50 +0100

    remove session cookies for API only app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_human-rights-national-reporting_session"
-Rails.application.config.middleware.use ActionDispatch::Cookies
-Rails.application.config.middleware.use ActionDispatch::Session::CookieStore, Rails.application.config.session_options
+Rails.application.config.session_store :disabled
