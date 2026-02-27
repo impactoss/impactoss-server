763e4d9cd8dac801442d52ce3f91d812629d3971
tmfrnz <tmfrnztimo@dumpark.com>
2026-02-27 11:25:02 +0100

    remove unnecessary headers from devise emails: read from instance variables; also add text part

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CustomDeviseMailer < Devise::Mailer
+  def reset_password_instructions(record, token, opts = {})
+    @redirect_url = opts[:redirect_url] || ""
+    @client_config = opts[:client_config] || "default"
+    super
+  end
+
   protected
 
   def headers_for(action, opts = {})
