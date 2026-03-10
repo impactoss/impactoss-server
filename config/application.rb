952ff5ee716a73d65f5e57507d50ed589eec7c92
tmfrnz <timo@dumpark.com>
2026-03-10 16:53:23 +0100

    allow localhost

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module HumanRightsNationalReporting
       end
 
       allow do
-        origins ENV.fetch("CLIENT_URL", "http://localhost:3000").chomp("/")
+        origins ENV.fetch("CLIENT_URL", "http://localhost:3000").chomp("/"), /\Ahttp:\/\/localhost(:\d+)?\z/
         resource "*",
           headers: :any,
           methods: :any,
