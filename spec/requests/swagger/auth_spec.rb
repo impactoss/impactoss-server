3db438c90e3b1af28c8db6cba26021cfbaf4e718
tmfrnz <timo@dumpark.com>
2026-03-20 14:31:51 +0100

    update Reset password tests

--- spec/requests/swagger/auth_spec.rb
+++ spec/requests/swagger/auth_spec.rb
@@ -165,6 +165,7 @@ RSpec.describe "Authentication API", type: :request do
       }
 
       response "200", "password updated" do
+        before { user.update(allow_password_change: true, reset_password_sent_at: Time.current) }
         let(:auth) { auth_headers_for(user) }
         let(:"access-token") { auth["access-token"] }
         let(:client) { auth["client"] }
@@ -175,11 +176,33 @@ RSpec.describe "Authentication API", type: :request do
       end
 
       response "422", "validation error (passwords don't match)" do
+        before { user.update(allow_password_change: true, reset_password_sent_at: Time.current) }
         let(:auth) { auth_headers_for(user) }
         let(:"access-token") { auth["access-token"] }
         let(:client) { auth["client"] }
         let(:uid) { auth["uid"] }
         let(:password_update) { {password: "Xk9#mP2$vL54!", password_confirmation: "Xk9#mP2$vL5x!"} }
+
+        run_test!
+      end
+
+      response "403", "password change not permitted without reset flow" do
+        let(:auth) { auth_headers_for(user) }
+        let(:"access-token") { auth["access-token"] }
+        let(:client) { auth["client"] }
+        let(:uid) { auth["uid"] }
+        let(:password_update) { {password: "Xk9#mP2$vL54!", password_confirmation: "Xk9#mP2$vL54!"} }
+
+        run_test!
+      end
+
+      response "403", "password change expired" do
+        before { user.update(allow_password_change: true, reset_password_sent_at: 2.hours.ago) }
+        let(:auth) { auth_headers_for(user) }
+        let(:"access-token") { auth["access-token"] }
+        let(:client) { auth["client"] }
+        let(:uid) { auth["uid"] }
+        let(:password_update) { {password: "Xk9#mP2$vL54!", password_confirmation: "Xk9#mP2$vL54!"} }
 
         run_test!
       end
