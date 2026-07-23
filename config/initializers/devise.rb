b1a7d829928846f39d99f08ddd8f50e0a0b245b1
tmfrnz <timo@dumpark.com>
2026-07-23 13:35:16 +0200

    Remove generator default overriding navigational_formats - not needed for API-app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,17 +231,6 @@ Devise.setup do |config|
   # only the current scope. By default, Devise signs out all scopes.
   # config.sign_out_all_scopes = true
 
-  # ==> Navigation configuration
-  # Lists the formats that should be treated as navigational. Formats like
-  # :html, should redirect to the sign in page when the user does not have
-  # access, but formats like :xml or :json, should return 401.
-  #
-  # If you have any extra navigational formats, like :iphone or :mobile, you
-  # should add them to the navigational formats lists.
-  #
-  # The "*/*" below is required to match Internet Explorer requests.
-  config.navigational_formats = ["*/*", :html]
-
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
