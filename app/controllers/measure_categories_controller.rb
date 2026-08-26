d43849bfbc547e13379a7512d3e201c2aac65b0e
tmfrnz <timo@dumpark.com>
2026-08-26 13:08:02 +0200

    Revert change for controllers using save_with_cleanup

--- app/controllers/measure_categories_controller.rb
+++ app/controllers/measure_categories_controller.rb
@@ -18,7 +18,7 @@ class MeasureCategoriesController < ApplicationController
     if @measure_category.save_with_cleanup
       render json: serialize(@measure_category), status: :created, location: @measure_category
     else
-      render_connection_create_errors(@measure_category)
+      render json: @measure_category.errors, status: :unprocessable_entity
     end
   end
 
